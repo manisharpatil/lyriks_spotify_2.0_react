b845af9d2ff5f98c63d1e462700cee5718d57671
manisharpatil <manisharpatil6@gmail.com>
2022-11-02 01:53:37 +0530

    feat: Discover component implements with Error, Loader SongCard components

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', '3fcbb2a8bbmsh67c3653024b1660p1ad2d9jsn9a11b13a5dea');
+
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => '/charts/world' }),
+    }),
+});
+
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi;
