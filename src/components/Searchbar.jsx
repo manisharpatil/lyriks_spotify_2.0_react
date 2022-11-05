f7d56ffed66e200bb3e20f6fefa18f587cc56a36
manisharpatil <manisharpatil6@gmail.com>
2022-11-06 00:12:41 +0530

    feat: sidebar functionality done and started dev for top play component

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,12 +1,6 @@
-import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import { RiCloseLine } from 'react-icons/ri';
-
-import { logo } from '../assets';
-import { links } from '../assets/constants';
-
-const Searchbar = () => {
-  const [ mobileMenuOpen, setMobileMenuOpen ] = useState(false);
-};
-
-export default Searchbar;
+const Searchbar = () => (
+    <div>Searchbar</div>
+  );
+  
+  export default Searchbar;
+  
