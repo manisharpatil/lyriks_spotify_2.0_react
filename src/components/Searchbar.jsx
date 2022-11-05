186b3a9bfbcb7af6b785b0dc112475163bc54aa5
manisharpatil <manisharpatil6@gmail.com>
2022-11-05 22:59:08 +0530

    feat: sidebar developement start

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,12 @@
-const Searchbar = () => (
-  <div>Loader</div>
-);
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { RiCloseLine } from 'react-icons/ri';
+
+import { logo } from '../assets';
+import { links } from '../assets/constants';
+
+const Searchbar = () => {
+  const [ mobileMenuOpen, setMobileMenuOpen ] = useState(false);
+};
 
 export default Searchbar;
