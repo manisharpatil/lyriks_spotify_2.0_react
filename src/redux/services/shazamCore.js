02dff3a7640f441112273c3279226d9812dba4e2
manisharpatil <manisharpatil6@gmail.com>
2022-11-12 01:17:55 +0530

    feat: songdetails and details header page added

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -12,7 +12,7 @@ export const shazamCoreApi = createApi({
     }),
     endpoints: (builder) => ({
         getTopCharts: builder.query({ query: () => '/charts/world' }),
-        getSongDetails: builder.query({ query: ({ songid }) => '/tracks/details?track_id=${songid}' }),
+        getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
     }),
 });
 
