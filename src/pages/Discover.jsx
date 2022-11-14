5980699bddbe420ca6e9bdf5147b6c16d6d50536
manisharpatil <manisharpatil6@gmail.com>
2022-11-15 00:57:40 +0530

    fear: final commit

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -11,17 +11,18 @@ const Discover = () => {
 
     const { data, isFetching, error } = useGetSongsByGenreQuery(genreListId || 'POP');
     
-    const genresTitle = 'Pop';
-
     if (isFetching) return <Loader title="Loading songs..." />
 
     if (error) return <Error />;
+
+    const genresTitle = genres.find(({ value }) => value === genreListId)?.title;
+
     return (
         <div className="flex flex-col">
             <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
                 <h2 className="font-bold text-3xl text-white text-left">Discover {genresTitle}</h2>
                 <select
-                    onChange={() => dispatch(selectGenreListId(e.target.value))}
+                    onChange={(e) => dispatch(selectGenreListId(e.target.value))}
                     value={genreListId || 'pop'}
                     className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5">
                     {genres.map((genres) => <option key={genres.value} value={genres.value}>{genres.title}</option>)}
